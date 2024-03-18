d56588f9b50597c09603fd371796975a2b8ee98b
Trent Smith <trsmith@1-E-32.42heilbronn.de>
2024-03-18 15:05:30 +0100

    done

--- ft_split.c
+++ ft_split.c
@@ -6,28 +6,28 @@
 /*   By: trsmith <trsmith@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 11:48:31 by trsmith           #+#    #+#             */
-/*   Updated: 2024/03/18 11:37:35 by trsmith          ###   ########.fr       */
+/*   Updated: 2024/03/18 14:58:32 by trsmith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_wcount(const char *s, char c)
+static int	ft_counter(const char *s, char c)
 {
 	int	count;
-	int	z;
+	int	i;
 
 	count = 0;
-	z = 0;
+	i = 0;
 	while (*s)
 	{
-		if (*s != c && z == 0)
+		if (*s != c && i == 0)
 		{
-			z = 1;
+			i = 1;
 			count++;
 		}
 		else if (*s == c)
-			z = 0;
+			i = 0;
 		s++;
 	}
 	return (count);
@@ -66,11 +66,11 @@ static void	*ft_free(char **strs, int count)
 	return (NULL);
 }
 
-static void	initialize(size_t *i, int *j, int *s_word)
+static void	initialize(size_t *i, int *j, int *sub_str)
 {
 	*i = 0;
 	*j = 0;
-	*s_word = -1;
+	*sub_str = -1;
 }
 
 char	**ft_split(const char *s, char c)
@@ -78,22 +78,22 @@ char	**ft_split(const char *s, char c)
 	char	**res;
 	size_t	i;
 	int		j;
-	int		s_word;
+	int		sub_str;
 
-	initialize(&i, &j, &s_word);
-	res = ft_calloc((ft_wcount(s, c) + 1), sizeof(char *));
+	initialize(&i, &j, &sub_str);
+	res = ft_calloc((ft_counter(s, c) + 1), sizeof(char *));
 	if (!res)
 		return (NULL);
 	while (i <= ft_strlen(s))
 	{
-		if (s[i] != c && s_word < 0)
-			s_word = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
+		if (s[i] != c && sub_str < 0)
+			sub_str = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && sub_str >= 0)
 		{
-			res[j] = fill_word(s, s_word, i);
+			res[j] = fill_word(s, sub_str, i);
 			if (!(res[j]))
 				return (ft_free(res, j));
-			s_word = -1;
+			sub_str = -1;
 			j++;
 		}
 		i++;
